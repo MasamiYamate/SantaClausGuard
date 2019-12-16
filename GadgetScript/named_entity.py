f8a826c75c2824a72e870f28cf0ed05fbd185f9b
MasamiYamate <m.yamate@mail.yamatte.net>
2019-12-16 13:50:16 +0900

    Script修正

---
+++ GadgetScript/named_entity.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+from polyglot.text import Text
+import cities
+
+# 日本が含まれているか確認する
+def match_japan(tags):
+    isMatch = False
+    for token in tags:
+        if token[0] == "Japan" and token[1] == "PROPN":
+            isMatch = True
+    return isMatch
+
+# 日本の都市名が含まれているか確認する
+def match_city(tokens, cities):
+    # 初期返却値はNone
+    result = None
+    for token in tokens:
+        # 固有名詞のときだけ処理する
+        if token[1] == "PROPN":
+            for city in cities:
+                print(city[0])
+                if city[0] == token[0]:
+                    result = city[1]
+    return result
+
+# 品詞タグ付けを行います
+def named_entity(text):
+    tokens = Text(text)
+    return tokens.pos_tags
